6f7d9fb27926211b96d76a79d6e07245af19ed06
Tony Wang <tiancheng_wang@brown.edu>
2020-08-02 16:53:36 -0400

    dynamo access

---
+++ cloudformation/dynamoClient.js
@@ -0,0 +1,54 @@
+// import entire SDK
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const config = require('./config.json');
+
+AWS.config.update(config);
+
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+      console.log("Region: ", AWS.config.region);
+    }
+});
+
+const input = fs.readFileSync('./input.md', {encoding:'utf8', flag:'r'});
+
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+const params = {
+    Item: {
+     "blogID": {
+        S: "1"
+     },
+     "commentID-replyID": {
+       S: "1-0"
+      }, 
+     "author": {
+       S: "Tony"
+      },
+      "mentionedID": {
+        S: "mmln"
+      },
+      "createTime": {
+        S: "10:00"
+      },
+      "editTime": {
+        S: "12:00"
+      },
+      "content": {
+        S: input
+      }
+    }, 
+    ReturnConsumedCapacity: "TOTAL", 
+    TableName: "RawComments"
+};
+
+dynamodb.putItem(params, (err, data) => {
+    console.log(err)
+})
+
+
+
---
+++ cloudformation/input.md
@@ -0,0 +1,17 @@
+# Hello
+
+$\sum_{i=0}^5 i$
+
+```python
+import time
+
+time.sleep(2)
+print("hello world")
+```
+
+中文支持：
+- 鬣
+
+### Avoid DOM-Based XSS Attack!
+
+<img src=1 onerror='console.log("xss")'>
